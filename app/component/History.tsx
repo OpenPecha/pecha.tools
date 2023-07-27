0f3854632210a5342f0a2cd3469f79a417c63075
kunsang <tenkus47@gmail.com>
2023-07-27 12:00:29 +0530

    added ql

--- app/component/History.tsx
+++ app/component/History.tsx
@@ -18,18 +18,18 @@ function History() {
 
 function EachHistory({ list }) {
   let data = list.data;
-  let accept = data.text.length;
-  let reject = data.rejected_list.length;
-  let ignore = data.ignored_list.length;
+  let accept = data?.text.length;
+  let reject = data?.rejected_list.length;
+  let ignore = data?.ignored_list.length;
 
   let showHistory = accept === 0 && reject === 0 && ignore === 0 ? false : true;
   if (!showHistory) return null;
   return (
     <div style={{ padding: 10 }}>
       <h4>{list.name}</h4>
-      <div>accept:{data.text.length}</div>
-      <div>reject:{data.rejected_list.length}</div>
-      <div>ignored:{data.ignored_list.length}</div>
+      <div>accept:{data?.text.length}</div>
+      <div>reject:{data?.rejected_list.length}</div>
+      <div>ignored:{data?.ignored_list.length}</div>
       <hr style={{ marginTop: 10 }} />
     </div>
   );
--- app/constant.ts
+++ app/constant.ts
@@ -7,4 +7,8 @@ export let toolList = [
     name: "sentense segmentation",
     url: "https://sentense-segmentation.onrender.com",
   },
+  {
+    name: "glyph annotation",
+    url: "https://pecha.tools/glyph_annotation/",
+  },
 ];
