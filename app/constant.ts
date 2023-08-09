773d9944278eb57a1c018f56bcf3a899ef2ee9c3
kunsang <tenkus47@gmail.com>
2023-08-09 19:50:26 +0530

    added background

--- app/constant.ts
+++ app/constant.ts
@@ -2,16 +2,21 @@ export let toolList = [
   {
     name: "word segmentor",
     url: "https://word.pecha.tools",
-    needUser: true,
+    needUser: false,
   },
   {
     name: "sentence segmentor",
     url: "https://sentence.pecha.tools",
-    needUser: true,
+    needUser: false,
   },
   {
-    name: "post editor",
-    url: "https://post-editor.pecha.tools",
+    name: "BO-EN post editor",
+    url: "https://post-editor.pecha.tools/bo2en",
+    needUser: false,
+  },
+  {
+    name: "EN-BO post editor",
+    url: "https://post-editor.pecha.tools/en2bo",
     needUser: false,
   },
 ];
