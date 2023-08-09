358bb35d455ef566f4c247ae012af8cbb526059b
kunsang <tenkus47@gmail.com>
2023-08-09 21:46:29 +0530

    add notification

--- app/component/Main.tsx
+++ app/component/Main.tsx
@@ -1,18 +1,59 @@
-import { Link, useLoaderData } from "@remix-run/react";
-
+import { Form, Link, useLoaderData } from "@remix-run/react";
+import { useState } from "react";
+let timer;
 function Main({ tools }) {
   const { user, status } = useLoaderData();
+  const [showMessage, setShowMessage] = useState(false);
+  function checkAuth() {
+    if (timer) clearTimeout(timer);
+    if (!user) {
+      setShowMessage(true);
+      timer = setTimeout(() => {
+        setShowMessage(false);
+      }, 3000);
+    }
+  }
   return (
     <main>
       <h1 className="tool-list-title">Annotation Tools </h1>
-      {!user && (
-        <div style={{ color: "red", marginBottom: 8 }}>login to use tools</div>
-      )}
+      {/* {!user && showMessage && ( */}
+      <div
+        className="bg-slate-800 text-white width-full px-4 py-2 capitalize"
+        style={{
+          display: "flex",
+          gap: 5,
+          position: "fixed",
+          bottom: 0,
+          justifyContent: "center",
+          transition: "all 0.3s ease-in-out",
+          alignItems: "center",
+          height: "50px",
+          zIndex: 1000,
+          transform: !showMessage ? "translateY(100%)" : "translateY(0)",
+        }}
+      >
+        <div>Login by</div>
+        <Form
+          action="/auth/auth0"
+          className="px-6 sm:px-0 max-w-sm text-blue-500"
+          method="post"
+        >
+          <button type="submit" className="hover:text-white">
+            {" "}
+            clicking here !!
+          </button>
+        </Form>
+      </div>
+      {/* )} */}
       <section>
         <div className="tool-list">
           {status.map((list) => {
             let disabled = list.needUser ? !user : false;
-            return <Tool list={list} key={list.name} disabled={disabled} />;
+            return (
+              <div onClick={checkAuth}>
+                <Tool list={list} key={list.name} disabled={disabled} />;
+              </div>
+            );
           })}
           {!tools?.error && <ProdigyTools tool={tools} />}
         </div>
