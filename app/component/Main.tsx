a946670f30f0bac6df4e61c1c24a562c723ea27c
kunsang <tenkus47@gmail.com>
2023-08-10 20:55:49 +0530

    ui Update

--- app/component/Main.tsx
+++ app/component/Main.tsx
@@ -15,13 +15,11 @@ function Main({ tools }) {
   }
   return (
     <main>
-      <h1 className="tool-list-title">Annotation Tools </h1>
+      <h1 className="text-xl font-bold">Annotation Tools </h1>
       {/* {!user && showMessage && ( */}
       <div
-        className="bg-slate-800 text-white width-full px-4 py-2 capitalize"
+        className="toast toast-start capitalize"
         style={{
-          display: "flex",
-          gap: 5,
           position: "fixed",
           bottom: 0,
           justifyContent: "center",
@@ -29,28 +27,32 @@ function Main({ tools }) {
           alignItems: "center",
           height: "50px",
           zIndex: 1000,
-          transform: !showMessage ? "translateY(100%)" : "translateY(0)",
+          transform: !showMessage ? "translateY(110%)" : "translateY(0)",
         }}
       >
-        <div>Login by</div>
-        <Form
-          action="/auth/auth0"
-          className="px-6 sm:px-0 max-w-sm text-blue-500"
-          method="post"
-        >
-          <button type="submit" className="hover:text-white">
-            {" "}
-            clicking here !!
-          </button>
-        </Form>
+        <div className="alert alert-info">
+          Login by
+          <Form
+            action="/auth/auth0"
+            className="px-6 sm:px-0 max-w-sm"
+            method="post"
+          >
+            <button
+              type="submit"
+              className="link text-red-700 hover:text-black animate-pulse"
+            >
+              clicking here !!
+            </button>
+          </Form>
+        </div>
       </div>
       {/* )} */}
       <section>
-        <div className="tool-list">
+        <div className="mx-auto grid lg:grid-cols-4 md:grid-cols-3 gap-5 px-[20px]">
           {status.map((list) => {
             let disabled = list.needUser ? !user : false;
             return (
-              <div onClick={checkAuth}>
+              <div onClick={checkAuth} className="text-center mx-auto">
                 <Tool list={list} key={list.name} disabled={disabled} />;
               </div>
             );
@@ -68,27 +70,31 @@ const linkStyle = {
 };
 function Tool({ list, disabled }) {
   return (
-    <div
-      className="tool-item min-w-fit "
-      style={{ opacity: disabled ? 0.4 : 1 }}
-    >
-      <Link to={"/tool/" + list.name.replace(" ", "_")} style={linkStyle}>
-        <div className={"tool-icon " + list.name}></div>
-        <div className="tool-name">{list.name}</div>
-      </Link>
-    </div>
+    <Link to={"/tool/" + list.name.replace(" ", "_")} style={linkStyle}>
+      <div
+        className="card w-30 bg-base-100 shadow-xl hover:scale-105 transition-all duration-300 ease-in-out"
+        style={{ opacity: disabled ? 0.4 : 1 }}
+      >
+        <figure className="hidden md:block">
+          <div className={"tool-icon " + list.name}></div>
+        </figure>
+        <div className="card-body uppercase text-center">{list.name}</div>
+      </div>
+    </Link>
   );
 }
 
 function ProdigyTools({ tool }) {
   if (!tool?.department) return null;
   return (
-    <div className="tool-item">
-      <Link to={"/tool/" + tool.department} style={linkStyle}>
-        <div className={"tool-icon " + tool.department}></div>
-        <div className="tool-name">{tool.department}</div>
-      </Link>
-    </div>
+    <Link to={"/tool/" + tool.department} style={linkStyle}>
+      <div className="card max-w-20 bg-base-100 shadow-xl hover:scale-105 transition-all duration-300 ease-in-out">
+        <figure className="hidden md:block">
+          <div className={"tool-icon " + tool.department}></div>
+        </figure>
+        <div className="card-body uppercase text-center">{tool.department}</div>
+      </div>
+    </Link>
   );
 }
 
