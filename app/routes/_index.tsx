6b19971de4f1786c74864b4536dcf51c9a85848f
kunsang <tenkus47@gmail.com>
2023-08-22 20:26:19 +0530

    add condition on email in getCombinetool

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -13,7 +13,7 @@ import { getCombineTools } from "~/utils/combineTools";
 
 export const loader: LoaderFunction = async ({ request }) => {
   let user = await getUserSession(request);
-  let toolList = await getCombineTools(user.email);
+  let toolList = await getCombineTools(user?.email);
   return defer({ user, tools: toolList });
 };
 
--- app/utils/combineTools.ts
+++ app/utils/combineTools.ts
@@ -2,10 +2,12 @@ import { fetchToolInfo } from "~/api/getUserToolInfo";
 import { toolList } from "~/constant";
 
 export async function getCombineTools(email: string) {
-  let updatedTool = await fetchToolInfo(email);
-  for (let i = 0; i < toolList.length; i++) {
-    if (toolList[i].department?.includes(updatedTool.department)) {
-      toolList[i].url = updatedTool.url;
+  if (email) {
+    let updatedTool = await fetchToolInfo(email);
+    for (let i = 0; i < toolList.length; i++) {
+      if (toolList[i].department?.includes(updatedTool.department)) {
+        toolList[i].url = updatedTool.url;
+      }
     }
   }
   return toolList;
