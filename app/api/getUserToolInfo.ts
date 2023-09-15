86ec3310584719718b9e21550a6ed89c03deedf6
kunsang <tenkus47@gmail.com>
2023-09-15 12:31:19 +0530

    updated Link to work pecha

--- app/api/getUserToolInfo.ts
+++ app/api/getUserToolInfo.ts
@@ -1,6 +1,6 @@
 export async function fetchToolInfo(email: string) {
   try {
-    let res = await fetch(`https://work.pecha.tools/api/mapping/${email}`);
+    let res = await fetch(`https://stt.pecha.tools/api/mapping/${email}`);
     let data = await res.json();
     return data;
   } catch (err) {
