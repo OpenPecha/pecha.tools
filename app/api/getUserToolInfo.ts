c3c63c053c2242c9f923665435318cbb27c66d47
kunsang <tenkus47@gmail.com>
2023-10-06 15:25:37 +0530

    Add redirect on error

--- app/api/getUserToolInfo.ts
+++ app/api/getUserToolInfo.ts
@@ -1,7 +1,11 @@
+import { redirect } from "@remix-run/node";
+
 export async function fetchToolInfo(email: string) {
   try {
     let res = await fetch(`https://stt.pecha.tools/api/mapping/${email}`);
+
     let data = await res.json();
+    if (data.error) redirect("/");
     return data;
   } catch (err) {
     return err;
