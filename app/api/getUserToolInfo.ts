2da42caf01d883597954271047e1189377487e6d
kunsang <tenkus47@gmail.com>
2023-10-06 15:28:31 +0530

    Add console.log on fetch

--- app/api/getUserToolInfo.ts
+++ app/api/getUserToolInfo.ts
@@ -3,8 +3,9 @@ import { redirect } from "@remix-run/node";
 export async function fetchToolInfo(email: string) {
   try {
     let res = await fetch(`https://stt.pecha.tools/api/mapping/${email}`);
-
     let data = await res.json();
+    console.log(data);
+
     if (data.error) redirect("/");
     return data;
   } catch (err) {
