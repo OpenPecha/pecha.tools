d89f5489b82cc34974a09362a598dcc33251e9b2
tenkus47 <tenkus47@gmail.com>
2025-01-23 15:05:19 +0530

    update login button

--- app/component/Header.tsx
+++ app/component/Header.tsx
@@ -4,8 +4,6 @@ import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import { Link, NavLink, useLoaderData } from "@remix-run/react";
 import Login from "./Login";
 
-const navigation = [];
-//   { name: "Dashboard", href: "#", current: true },
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -13,7 +11,6 @@ function classNames(...classes) {
 
 export default function Header() {
   const { user } = useLoaderData();
-  const isAdmin = user?.role === "admin";
   return (
     <Disclosure
       as="nav"
@@ -42,25 +39,6 @@ export default function Header() {
                   </NavLink>
                 </div>
 
-                <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current
-                            ? "bg-gray-900 text-white"
-                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                          "rounded-md px-3 py-2 text-sm font-medium"
-                        )}
-                        aria-current={item.current ? "page" : undefined}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
-                  </div>
-                </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 {user ? (
@@ -98,20 +76,7 @@ export default function Header() {
                             </div>
                           )}
                         </Menu.Item>
-                        <Menu.Item>
-                          {({ active }) => (
-                            <Link
-                              to={"/dashboard"}
-                              className={classNames(
-                                isAdmin ? "" : "hidden",
-                                active ? "bg-gray-100" : "",
-                                "block px-4 py-2 text-sm text-gray-700"
-                              )}
-                            >
-                              Dashboard
-                            </Link>
-                          )}
-                        </Menu.Item>
+                   
                         <Menu.Item>
                           {({ active }) => (
                             <Link
@@ -134,27 +99,6 @@ export default function Header() {
               </div>
             </div>
           </div>
-
-          <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? "bg-gray-900 text-white"
-                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                    "block rounded-md px-3 py-2 text-base font-medium"
-                  )}
-                  aria-current={item.current ? "page" : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
-            </div>
-          </Disclosure.Panel>
         </>
       )}
     </Disclosure>
--- app/component/Login.tsx
+++ app/component/Login.tsx
@@ -18,21 +18,6 @@ import { twMerge } from "tailwind-merge";
 // This form would take us to the auth0 route, which would redirect to the Auth0 login page.
 
 export default function Login() {
-  return (
-    <Dialog>
-      <DialogTrigger asChild className="btn btn-sm btn-primary">
-        <Button className="bg-blue-700 hover:bg-blue-600" id="login-btn">
-          LOG IN
-        </Button>
-      </DialogTrigger>
-      <DialogContent className="modal m-auto max-w-sm">
-        <LoginForm />
-      </DialogContent>
-    </Dialog>
-  );
-}
-
-function LoginForm() {
   let { env } = useLoaderData();
   let { AUTH0_DOMAIN, AUTH0_CLIENT_ID, NODE_ENV } = env;
   let location =
@@ -45,59 +30,16 @@ function LoginForm() {
     scope: "email profile openid",
   };
 
-  const { loginWithGoogle, passwordlessLogin, verifyPasswordlessCode } =
+  const { loginWithGoogle} =
     useAuth0(auth0Config);
-  const [email, setEmail] = useState("");
-  const [redirectEmail, setRedirectEmail] = useState(null);
-  const inputRef = useRef<HTMLInputElement>(null);
-  const [showOtpField, setShowOtpField] = useState(false);
-  // Passwordless login
-  const handlePasswordlessLogin = async (e) => {
-    e.preventDefault();
-    try {
-      const resEmail = await passwordlessLogin(email);
-      if (resEmail) {
-        setShowOtpField(true);
-        setRedirectEmail(resEmail);
-      }
-    } catch (err) {
-      console.log("err: ", err);
-    }
-  };
+  
 
   // Login with Google
   const handleGoogleLogin = () => {
     loginWithGoogle();
   };
-  async function handleLoginPassword(e) {
-    e.preventDefault();
-    //access code
-    let code = parseInt(inputRef.current?.value);
-    try {
-      let res = await verifyPasswordlessCode(redirectEmail, code);
-      console.log(res);
-    } catch (err) {
-      console.log("code verification error : ", err);
-    }
-  }
+ 
   return (
-    <>
-      <DialogHeader>
-        <img
-          src="/favicon/favicon.png"
-          className="mx-auto w-auto"
-          style={{ height: "15vh" }}
-        />
-        <DialogTitle className="mx-auto">Pecha tools</DialogTitle>
-        <DialogDescription className="mx-auto">
-          Log in to Pecha tools to continue using tools
-        </DialogDescription>
-      </DialogHeader>
-
-      {redirectEmail ? (
-        <div></div>
-      ) : (
-        <>
           <Button
             onClick={handleGoogleLogin}
             className="text-white w-fit mx-auto bg-blue-600 hover:bg-blue-700 "
@@ -105,114 +47,5 @@ function LoginForm() {
             <FcGoogle className="text-xl" />
             <span className="text-lg font-semibold leading-6 ml-3">Google</span>
           </Button>
-          <div className="relative mt-3 ">
-            <div
-              className="absolute inset-0 flex items-center"
-              aria-hidden="true"
-            >
-              <div className="w-full border-t border-stone-200"></div>
-            </div>
-            <div className="relative flex justify-center text-sm font-regular leading-6">
-              <span className="bg-white px-6 text-stone-500">
-                Or sign in with
-              </span>
-            </div>
-          </div>
-        </>
-      )}
-      {!showOtpField ? (
-        <form onSubmit={handlePasswordlessLogin}>
-          <div className="mb-8 mx-auto">
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium leading-6 text-stone-900 dark:text-gray-300"
-            >
-              Email address
-            </label>
-            <div className="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                required
-                autoFocus
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="block w-full rounded-md border-0 py-1.5 text-stone-900 dark:text-gray-300 pl-2 shadow-sm ring-1 ring-inset ring-stone-300 placeholder:text-stone-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-          <div>
-            <button
-              type="submit"
-              disabled={true}
-              className="flex w-full justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600 transition"
-            >
-              Continue
-            </button>
-          </div>
-        </form>
-      ) : (
-        <>
-          <div>
-            <h3 className="font-semibold mb-2 text-primary-800">
-              Check your inbox
-            </h3>
-            <p className="text-sm text-stone-700/70">
-              We have sent you a magic link. If you did not receive the email,
-              <button
-                onClick={handlePasswordlessLogin}
-                type="button"
-                className="font-medium underline underline-offset-2 decoration-primary-700/25 hover:text-primary-700 hover:decoration-primary-700/50 transition"
-              >
-                send a new one
-              </button>
-              .
-            </p>
-          </div>
-          <div className="mt-6 flex flex-col">
-            <form onSubmit={handleLoginPassword}>
-              <div className="mb-4">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-stone-900"
-                >
-                  Enter code
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="password"
-                    ref={inputRef}
-                    name="password"
-                    type="password"
-                    required
-                    autoFocus
-                    className="block w-full rounded-md border-0 py-1.5 text-stone-900 shadow-sm ring-1 ring-inset ring-stone-300 placeholder:text-stone-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-              <div className="mb-8">
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600 transition"
-                >
-                  Sign in
-                </button>
-                <button
-                  type="button"
-                  onClick={() => {
-                    setRedirectEmail(null);
-                    setShowOtpField(false);
-                  }}
-                >
-                  go back
-                </button>
-              </div>
-            </form>
-          </div>
-        </>
-      )}
-    </>
   );
 }
